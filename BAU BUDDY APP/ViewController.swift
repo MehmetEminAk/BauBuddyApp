ba5643bdc0636f526b7eb36355d3e3ae0d4f6133
Mehmet Emin Ak <mak44162@gmail.com>
2023-03-01 17:39:40 +0300

    Initial Commit

---
+++ BAU BUDDY APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BAU BUDDY APP
+//
+//  Created by Macbook Air on 1.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BAU BUDDY APPUITests/BAU_BUDDY_APPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BAU_BUDDY_APPUITestsLaunchTests.swift
+//  BAU BUDDY APPUITests
+//
+//  Created by Macbook Air on 1.03.2023.
+//
+
+import XCTest
+
+final class BAU_BUDDY_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
